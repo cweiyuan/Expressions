1253779ddf40fb157db83514bad4b462eab26993
cweiyuan <93870563+cweiyuan@users.noreply.github.com>
2022-06-10 01:51:22 +0800

    Question 3
    Rewrite the program in Question 2 so that it prints the reversal of a three-digit number without using arithmetic to split the number into digits.

---
+++ Question3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    printf("Enter a three-digit number: ");
+    scanf("%d%d%d", &i, &j, &k);
+    printf("The reversal is: %d%d%d", i, j, k);
+}
