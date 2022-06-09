700e13de13af3b3c94295bf799d0b1cb7fde27d1
cweiyuan <93870563+cweiyuan@users.noreply.github.com>
2022-06-10 01:48:12 +0800

    Question 1
    Write a program that asks the user to enter a two-digit number, then prints the number with its digits reversed. A session with the program should have the following appearance.

---
+++ Question1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    printf("Enter a two-digit number: ");
+    scanf("%d", &i);
+
+    j = i % 10;
+    k = i / 10;
+    
+    printf("The reversal is: %d%d",j, k);
+
+    return 0;
+}
